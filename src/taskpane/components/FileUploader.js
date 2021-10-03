b2a80d2be3ec9f9b02577ec29153b9bc08c059e7
clockvoid <hidenari.t1997nov@gmail.com>
2021-10-04 04:40:30 +0900

    add: upload this file button to file uploader

--- src/taskpane/components/FileUploader.js
+++ src/taskpane/components/FileUploader.js
@@ -28,6 +28,80 @@ const FileUploader = (props) => {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [plainFiles]);
 
+  const getAllSlices = (file) => {
+    var isError = false;
+
+    return new Promise(async (resolve, reject) => {
+      var documentFileData = [];
+      for (var sliceIndex = 0; (sliceIndex < file.sliceCount) && !isError; sliceIndex++) {
+        var sliceReadPromise = new Promise((sliceResolve, sliceReject) => {
+          file.getSliceAsync(sliceIndex, (asyncResult) => {
+            if (asyncResult.status === Office.AsyncResultStatus.Succeeded) {
+              documentFileData = documentFileData.concat(asyncResult.value.data);
+              sliceResolve({
+                IsSuccess: true,
+                Data: documentFileData
+              });
+            } else {
+              file.closeAsync();
+              sliceReject({
+                IsSuccess: false,
+                ErrorMessage: `Error in reading the slice: ${sliceIndex} of the document`
+              });
+            }
+          });
+        });
+        await sliceReadPromise.catch((error) => {
+          isError = true;
+        });
+      }
+
+      if (isError || !documentFileData.length) {
+        reject('Error while reading document. Please try it again.');
+        return;
+      }
+
+      file.closeAsync();
+
+      resolve({
+        IsSuccess: true,
+        Data: documentFileData
+      });
+    });
+  }
+
+  const getCurrentFile = async() => {
+    try {
+      await Excel.run(async (context) => {
+        var title = decodeURI(Office.context.document.url).split('/').pop().split('\\').pop();
+        Office.context.document.getFileAsync(Office.FileType.Compressed, 
+          (result) => {
+            if (result.status == "succeeded") {
+              var myFile = result.value;
+              console.log(myFile);
+              getAllSlices(myFile).then(
+                (result) => {
+                  console.log(result);
+                  let file = new Blob([new Uint8Array(result.Data)], { type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"});
+                  file['fileName'] = title;
+                  props.setFile(file);
+                },
+                (reject) => {
+                  console.log(reject);
+                }
+              )
+            } else {
+              console.log(result.error.message);
+            }
+        });
+
+        return context.sync();
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <div className="fileUploader">
       <DragAndDrop handleDrop={handleDrop}>
@@ -35,6 +109,7 @@ const FileUploader = (props) => {
         <h2 className="fileUploaderHeadline">ファイルをアップロードする</h2>
         <p className="fileUploadertext">CSV，Excel，PDFをドラッグ&ドロップして，形式をチェックします</p>
         <button className="fileUploaderbutton" onClick={openFileSelector}>ファイルを選択</button>
+        <button className="fileUploaderbutton" onClick={getCurrentFile}>このファイルをアップロード</button>
       </DragAndDrop>
     </div>
   );
