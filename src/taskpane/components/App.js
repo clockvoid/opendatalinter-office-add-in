adaf6d441b9e85a4d0f4ba058ecb92250f64c796
clockvoid <hidenari.t1997nov@gmail.com>
2021-10-09 02:36:44 +0900

    fix: app.js

--- src/taskpane/components/App.js
+++ src/taskpane/components/App.js
@@ -4,8 +4,6 @@ import "../../../assets/icon-16.png";
 import "../../../assets/icon-32.png";
 import "../../../assets/icon-80.png";
 
-import { useEffect, useState } from 'react';
-import FileUploader from './FileUploader';
 import UploadProgress from './UploadProgress';
 import UnableToLoadFile from './UnableToLoadFile';
 import ResultList from './ResultList.js';
@@ -50,7 +48,9 @@ export default class App extends React.Component {
   }
 
   componentDidMount() {
-    this.updateCurrentFile(this);
+    Office.initialize = () => {
+      this.updateCurrentFile(this);
+    }
   }
 
   componentDidUpdate(_, prevState) {
@@ -99,7 +99,7 @@ export default class App extends React.Component {
         <main className="main">
           <div className="mainInner">
             { this.state.mode === UploadMode && <UploadProgress uploadProgress={this.state.uploadProgress} file={this.state.file} /> }
-            { this.state.mode === ErrorMode && <UnableToLoadFile /> }
+            { this.state.mode === ErrorMode && <UnableToLoadFile updateCurrentFile={() => this.updateCurrentFile(this)} /> }
             { this.state.mode === ResultMode && <ResultList results={this.state.results} file={this.state.file} goBack={() => this.updateCurrentFile(this)} /> }
           </div>
         </main>
@@ -114,100 +114,4 @@ export default class App extends React.Component {
     );
   }
 }
-
-//function App() {
-//
-//  const [file, setFile] = useState(undefined);
-//  const [uploadProgress, setUploadProgress] = useState(0);
-//  const [results, setResults] = useState([]);
-//  const [initialResult, setInitialResult] = useState([]);
-//  const [mode, setMode] = useState(InitialMode);
-//
-//  const sleep = (ms) => {
-//    return new Promise(resolve => setTimeout(resolve, ms));
-//  }
-//
-//  const reset = () => {
-//    setMode(InitialMode);
-//    axios.request({
-//      method: 'get',
-//      url: 'https://opendatalinter.volare.site/'
-//    }).then(data => {
-//      setInitialResult(data.data);
-//    });
-//    setUploadProgress(0);
-//  }
-//
-//  useEffect(() => {
-//    if (file === undefined || file === null) {
-//      return;
-//    }
-//
-//    console.log(file);
-//
-//    setMode(UploadMode);
-//    setResults(initialResult);
-//
-//    const submitData = new FormData();
-//    submitData.append("file", file, file.fileName);
-//    axios.request({
-//      method: 'post',
-//      url: 'https://opendatalinter.volare.site/',
-//      data: submitData,
-//      onUploadProgress: (e) => {
-//        setUploadProgress(e.loaded / e.total * 100);
-//        if (e.loaded === e.total) {
-//          sleep(50).then(() => {
-//            setMode(ResultMode);
-//          });
-//        }
-//      }
-//    }).then(data => {
-//      setResults(data.data);
-//    }).catch(error => {
-//    });
-//  // setInitialResultとhistoryを更新しているが，無限ループにはならないので無視
-//  // eslint-disable-next-line react-hooks/exhaustive-deps
-//  }, [file]);
-//
-//  const displayMessageOnActivated = async () => {
-//    try {
-//      await Excel.run(async (context) => {
-//        var sheet = context.workbook.worksheets.getFirst();
-//
-//        sheet.onActivated.add((event) => {
-//          return Excel.run((context) => {
-//            console.log(event.worksheetId);
-//            return context.sync();
-//          });
-//        });
-//
-//        return context.sync();
-//      });
-//    } catch (error) {
-//      console.log(error);
-//    }
-//  }
-//
-//  return (
-//    <div className="App">
-//      <main className="main">
-//        <div className="mainInner">
-//          { mode === UploadMode && <UploadProgress uploadProgress={uploadProgress} file={file} /> }
-//          { mode === InitialMode && <FileUploader setFile={setFile} /> }
-//          { mode === ResultMode && <ResultList results={results} file={file} goBack={reset} /> }
-//        </div>
-//      </main>
-//      <footer className="footer">
-//        <p className="footerText">
-//          総務省から提供されている
-//          <a href="https://www.soumu.go.jp/main_content/000723626.pdf" target="_blank" rel="noopener noreferrer" className="footerLink">機械判読可能な統計表の統一ルール</a>
-//          を基に形式をチェックしています
-//        </p>
-//      </footer>
-//    </div>
-//  );
-//}
-//
-//export default App;
 
